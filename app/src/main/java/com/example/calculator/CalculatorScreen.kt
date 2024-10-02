36c9868974db6b76863fdab25789ad0f7c6f4e2d
TigasGPT <tmdmiranda@gmail.com>
2024-10-02 12:39:23 +0100

    button "/"

--- app/src/main/java/com/example/calculator/CalculatorScreen.kt
+++ app/src/main/java/com/example/calculator/CalculatorScreen.kt
@@ -3,61 +3,99 @@ package com.example.calculator
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculator.ui.theme.CalculatorTheme
 import com.example.calculator.ui.theme.components.CalcButton
 
+
 @Composable
 fun CalcScreen(modifier: Modifier = Modifier) {
+
+
+    var displayText by remember { mutableStateOf("0") }
+    val onNumPressed : (String) -> Unit = { num ->
+        if (displayText == "0") {
+            displayText = num
+        }else{
+            displayText += num
+        }
+    }
+
     Column(modifier = modifier) {
-        Text(text = "0")
-        Row(
-            modifier = modifier
-                .weight(1f)
-                .aspectRatio(5f)
-
-        ) {
-            CalcButton(modifier = modifier.weight(1f), "7", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "8", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "9", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "x", isOperation = true, onClick = {})
-        }
-        Row(
-            modifier = modifier
-                .weight(1f)
-                .aspectRatio(5f)
-        ) {
-            CalcButton(modifier = modifier.weight(1f), "4", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "5", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "6", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "x", isOperation = true, onClick = {})
-        }
-        Row(
-            modifier = modifier
-                .weight(1f)
-                .aspectRatio(5f)
+        Text(text = displayText,
+        modifier = Modifier
+            .fillMaxSize()
+            .weight(1f),
+            style = MaterialTheme.typography.displayLarge,
+            textAlign = TextAlign.End
         )
-        {
-            CalcButton(modifier = modifier.weight(1f), "1", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "2", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "3", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "x", isOperation = true, onClick = {})
+        Row(
+            modifier = modifier
+                .weight(1f)
+
+
+        ){
+            CalcButton(modifier = modifier.size(100.dp,100.dp), "/", isOperation = true, onClick = {})
+
 
         }
+
         Row(
             modifier = modifier
                 .weight(1f)
-                .aspectRatio(5f)
+
+
+        ) {
+            CalcButton(modifier = modifier.weight(1f), "7", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "8", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "9", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "x", isOperation = true, onClick = {})
+        }
+        Row(
+            modifier = modifier
+                .weight(1f)
+                
+        ) {
+            CalcButton(modifier = modifier.weight(1f), "4", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "5", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "6", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "+", isOperation = true, onClick = {})
+        }
+        Row(
+            modifier = modifier
+                .weight(1f)
         )
         {
-            CalcButton(modifier = modifier.weight(1f), "+/-", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "0", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), ".", onClick = {/*TODO*/ })
-            CalcButton(modifier = modifier.weight(1f), "0", isOperation = true, onClick = {})
+            CalcButton(modifier = modifier.weight(1f), "1", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "2", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "3", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), "-", isOperation = true, onClick = {})
+
+        }
+        Row(
+            modifier = modifier
+                .weight(1f)
+
+        )
+        {
+            CalcButton(modifier = modifier.weight(1f), "C", true, onClick = {displayText = "0"})
+            CalcButton(modifier = modifier.weight(1f), "0", onClick = onNumPressed)
+            CalcButton(modifier = modifier.weight(1f), ".", true, onClick = {/*TODO*/ })
+            CalcButton(modifier = modifier.weight(1f), "=", true, onClick = {})
 
         }
 
