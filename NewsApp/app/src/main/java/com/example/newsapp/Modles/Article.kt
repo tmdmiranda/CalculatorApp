dc9ec790ccb5a3c554600a9db00558b1130408fd
TigasGPT <tmdmiranda@gmail.com>
2024-10-15 09:41:47 +0100

    5

---
+++ NewsApp/app/src/main/java/com/example/newsapp/Modles/Article.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.Modles
+
+import java.util.Date
+
+class Article
+    (var title: String? = null,
+    var description : String?= null,
+    var urlToImage : String?= null,
+    var url: String?= null,
+    var publishedAt: Date? = null){
+
+}
---
+++ NewsApp/app/src/main/java/com/example/newsapp/ui/HomeView.kt
@@ -0,0 +1,39 @@
+package com.example.newsapp.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.newsapp.Modles.Article
+import java.util.Date
+
+@Composable
+fun HomeView( modifier: Modifier = Modifier) {
+
+    var articles = arrayListOf(
+        Article(
+            "dsflsdlfk selkfwçlfmkqw",
+            "description",
+            "urlToImage",
+            "url",
+            Date()),
+        Article(
+            "WEFWEFWEFWF ",
+            "description",
+            "urlToImage",
+            "url",
+            Date()),
+
+        )
+
+    LazyColumn {
+        itemsIndexed(
+            items = articles,
+        ){
+                index, article ->
+            Text(text = article.title!!)
+        }
+
+    }
+}
